8f0f89d0a17918df75ee89d2f1c98abf2097350f
Tenor Biel <tenorbiel@gmail.com>
2015-02-08 21:57:32 -0600

    implement GET handler for User

--- lib/Cido/Queries/User.hs
+++ lib/Cido/Queries/User.hs
@@ -2,6 +2,7 @@
 
 module Cido.Queries.User ( getAllUsers
                          , authenticateUser
+                         , findUserById
                          ) where
 
 import Data.Functor      ((<$>))
@@ -9,7 +10,7 @@ import Hasql
 import Data.Text         (Text)
 
 import Cido.Types.Server
-import Cido.Types.User   (User(User), AuthenticatedUser(..))
+import Cido.Types.User   hiding (id, username)
 
 getAllUsers :: Int -> Int -> forall s. Query s [User]
 getAllUsers o l = map fromRow <$> listEx [stmt|
@@ -19,7 +20,14 @@ getAllUsers o l = map fromRow <$> listEx [stmt|
         LIMIT $l
     |]
 
-fromRow :: (Int, Text) -> User
+findUserById :: Id -> forall s. Query s (Maybe User)
+findUserById uid = fmap fromRow <$> maybeEx [stmt|
+        SELECT id, username
+        FROM users
+        WHERE id = $uid
+    |]
+
+fromRow :: (Id, Username) -> User
 fromRow (uid, username) = User uid username
 
 authenticateUser :: User -> Text -> forall s. Query s (Maybe AuthenticatedUser)
--- lib/Cido/Resources/User.hs
+++ lib/Cido/Resources/User.hs
@@ -1,5 +1,6 @@
 module Cido.Resources.User (resource) where
 
+import Control.Monad.Error  (ErrorT, throwError)
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Trans  (lift)
 
@@ -7,19 +8,30 @@ import Rest
 import qualified Rest.Resource as R
 
 import Cido.Types.Server
+import Cido.Types.User      (User)
 import Cido.Queries.User
 
-data UserId = ById Int
+data Identifier = ById Int
 
-type WithUserId = ReaderT UserId Api
+type WithUser = ReaderT Identifier Api
 
-resource :: Resource Api WithUserId UserId () Void
+resource :: Resource Api WithUser Identifier () Void
 resource = mkResourceReader
   { R.name   = "users"
   , R.schema = withListing () $ named [("id", singleRead ById)]
   , R.list   = const listUsers
+  , R.get    = Just getUser
   }
 
 listUsers :: ListHandler Api
-listUsers = mkListing (jsonO . someO) handle where
-    handle (Range o l) = lift $ runQuery $ getAllUsers o l
+listUsers = mkListing (jsonO . someO) handler where
+    handler (Range o l) = lift $ runQuery $ getAllUsers o l
+
+getUser :: Handler WithUser
+getUser = mkIdHandler (jsonO . someO) handler where
+    handler :: () -> Identifier -> ErrorT Reason_ WithUser User
+    handler _ (ById uid) = do
+        r <- lift $ lift $ runQuery $ findUserById uid
+        case r of
+            Just u  -> return u
+            Nothing -> throwError NotFound
