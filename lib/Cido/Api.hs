a19c09bcbb24c21bcc4fd0e8ab8700209f7a67cb
Tenor Biel <tenorbiel@gmail.com>
2015-02-09 23:06:43 -0600

    add NotFound fallback route

--- lib/Cido/Api.hs
+++ lib/Cido/Api.hs
@@ -2,11 +2,14 @@ module Cido.Api (api) where
 
 import Happstack.Server
 import Control.Monad       (msum)
-import Cido.Types          (Api)
+import Control.Monad.Error (throwError)
+
+import Cido.Types
 
 import qualified Cido.Api.User as User
 
 api :: Api Response
 api = msum
     [ dir "users" User.api
+    , throwError NotFound
     ]
