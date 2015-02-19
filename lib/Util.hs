2c8088a823adaa8bd5a460b058a22beed3f17d30
Tenor Biel <tenorbiel@gmail.com>
2015-02-18 23:35:53 -0600

    substitute uuid-aeson package for Aeson UUID instances

--- lib/Util.hs
+++ lib/Util.hs
@@ -7,18 +7,9 @@ import Happstack.Server
 import Data.Aeson
 import Data.Aeson.Encode       (encodeToByteStringBuilder)
 import Data.ByteString.Builder (toLazyByteString)
-import Data.UUID
-import Data.Text
+import Data.UUID.Aeson         ()
 
 instance ToMessage Value where
     toContentType _ = "application/json; charset=utf-8"
     toMessage = toLazyByteString . encodeToByteStringBuilder
     toResponse x = toResponseBS (toContentType x) (toMessage x)
-
-instance ToJSON UUID where
-    toJSON = String . pack . toString
-
-instance FromJSON UUID where
-    parseJSON = withText "UUID" $ maybe (fail "could not parse UUID") return
-                                . fromString
-                                . unpack
